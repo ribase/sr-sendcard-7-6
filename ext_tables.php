01065539f669d0b1af0999c71497895d829d6408
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-14 05:56:07 +0000

    Remove reference to deprecated method
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_sendcard/trunk@84258 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,7 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
+$typo3Version = t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version);
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'static/css_styled/', 'Send-A-Card CSS-styled');
 
@@ -28,7 +29,9 @@ $GLOBALS['TCA']['tx_srsendcard_card'] = Array (
 t3lib_extMgm::allowTableOnStandardPages('tx_srsendcard_card');
 t3lib_extMgm::addToInsertRecords('tx_srsendcard_card');
 
-t3lib_div::loadTCA('tt_content');
+if ($typo3Version < 6001000) {
+	t3lib_div::loadTCA('tt_content');
+}
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1'] = 'layout,select_key';
 t3lib_extMgm::addPlugin(Array('LLL:EXT:sr_sendcard/locallang_db.xml:tt_content.list_type', $_EXTKEY.'_pi1'), 'list_type');
 
@@ -36,4 +39,5 @@ if (TYPO3_MODE == 'BE') {
 	$GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['tx_srsendcard_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_srsendcard_pi1_wizicon.php';
 	t3lib_extMgm::addModule('web', 'txsrsendcardM1', '', t3lib_extMgm::extPath($_EXTKEY).'mod1/');
 }
+unset($typo3Version);
 ?>
